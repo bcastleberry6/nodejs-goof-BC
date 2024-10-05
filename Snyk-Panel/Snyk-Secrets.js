1161f40062d872b1abf461b0923a73b614f61c4a
Brooke Castleberry <brookecastleberry@Brookes-MBP.lan>
2024-10-04 20:58:35 -0400

    initial commit

---
+++ Snyk-Panel/Snyk-Secrets.js
@@ -0,0 +1,15 @@
+// secrets.js
+const secrets = {
+    apiKey: "YOUR_API_KEY",
+    dbPassword: "YOUR_DB_PASSWORD"
+};
+
+module.exports = secrets;
+
+const secrets_2 = {
+    apiKey: "YOUR_API_KEY_2",
+    dbPassword: "YOUR_DB_PASSWORD_2"
+};
+
+module.exports = secrets;
+
