44ffc8fbb9ae8d81499ee4309454821a16236893
Brooke Castleberry <brookecastleberry@Brookes-MBP.lan>
2024-10-05 20:49:42 -0400

    commit change to two files

--- Snyk-Panel/Snyk-Secrets.js
+++ Snyk-Panel/Snyk-Secrets.js
@@ -1,46 +1,13 @@
 // secrets.js
-const secrets = {
-    apiKey: "YOUR_API_KEY",
-    dbPassword: "YOUR_DB_PASSWORD"
-};
+//const secrets = {
+//    apiKey: "YOUR_API_KEY",
+//    dbPassword: "YOUR_DB_PASSWORD"
+//};
 
-module.exports = secrets;
+//module.exports = secrets;
 
-const secrets_2 = {
-    apiKey: "YOUR_API_KEY_2",
-    dbPassword: "YOUR_DB_PASSWORD_2"
-};
+aws_access_key_id = AKIA2UC3BSXMKPUGS2OL
+aws_secret_access_key = cQJuCUVa6fWmiehc1o3lNCNg8Jm4jrv2CkB+4+40
+output = json
+region = us-east-2
 
-module.exports = secrets;
-
-const secrets_3 = {
-    apiKey: "YOUR_API_KEY_3",
-    dbPassword: "YOUR_DB_PASSWORD_3"
-};
-
-module.exports = secrets;
-
-const secrets_4 = {
-    apiKey: "YOUR_API_KEY_4",
-    dbPassword: "YOUR_DB_PASSWORD_4"
-};
-
-module.exports = secrets;
-
-
-const secrets_5 = {
-    apiKey: "YOUR_API_KEY_5",
-    dbPassword: "YOUR_DB_PASSWORD_5",
-    
-};
-
-module.exports = secrets;
-
-
-const secrets_6 = {
-    apiKey: "YOUR_API_KEY_6",
-    dbPassword: "YOUR_DB_PASSWORD_6",
-    
-};
-
-module.exports = secrets;
