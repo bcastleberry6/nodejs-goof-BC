4db7934bc1eadd38e414c4a609aaa4ddce20160c
Brooke Castleberry <brookecastleberry@Brookes-MBP.lan>
2024-10-05 22:46:13 -0400

    update 2 files

--- Snyk-Panel/Snyk-Secrets.js
+++ Snyk-Panel/Snyk-Secrets.js
@@ -6,8 +6,8 @@
 
 //module.exports = secrets;
 
-aws_access_key_id = AKIA2UC3BSXMIXS35EWO
-aws_secret_access_key = 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
+aws_access_key_id = AKIA2UC3BSXMI4VBQHWT
+aws_secret_access_key = 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
 output = json
 region = us-east-2
 
