03f48f9b71891cf96ed5b17ee05bbc26c650d26b
Brooke Castleberry <brookecastleberry@Brookes-MBP.lan>
2024-10-06 15:03:39 -0400

    commit change to secrets: git push -u origin main

--- Snyk-Panel/Snyk-Secrets.js
+++ Snyk-Panel/Snyk-Secrets.js
@@ -6,7 +6,7 @@
 
 //module.exports = secrets;
 
-aws_access_key_id = AKIA2UC3BSXMBSOYQ74F
-aws_secret_access_key = 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
+aws_access_key_id = AKIA2UC3BSXMCFUN2QMU
+aws_secret_access_key = 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
 output = json
 region = us-east-2
