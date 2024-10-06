b610e062b9f40af9654abf430d84c14e4853243a
Brooke Castleberry <brookecastleberry@Brookes-MBP.lan>
2024-10-06 10:27:48 -0400

    commit change

--- Snyk-Panel/Snyk-Secrets.js
+++ Snyk-Panel/Snyk-Secrets.js
@@ -6,8 +6,8 @@
 
 //module.exports = secrets;
 
-aws_access_key_id = AKIA2UC3BSXMDBGTOUCK
-aws_secret_access_key = 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
+aws_access_key_id = AKIA2UC3BSXMLY72DALT
+aws_secret_access_key = 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
 output = json
 region = us-east-2
 
