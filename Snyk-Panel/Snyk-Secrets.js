da4fad3d95410b30c5c8a000268c1e63977cf769
Brooke Castleberry <brookecastleberry@Brookes-MBP.lan>
2024-10-08 22:09:22 -0400

    commit changes to Secrets file

--- Snyk-Panel/Snyk-Secrets.js
+++ Snyk-Panel/Snyk-Secrets.js
@@ -1,11 +1,3 @@
-// secrets.js
-//const secrets = {
-//    apiKey: "YOUR_API_KEY",
-//    dbPassword: "YOUR_DB_PASSWORD"
-//};
-
-//module.exports = secrets;
-
 aws_access_key_id = AKIA2UC3BSXMODCO6HHQ
 aws_secret_access_key = f5SmAGOrAncRIaJLFQAXMK6d8IRJLERO2/j5VfRt
 output = json
