06e894471c1f1b73c4b08e777e6111a6e746e3ec
Brooke Castleberry <brookecastleberry@Brookes-MBP.lan>
2024-10-09 10:02:00 -0400

    commit changes to secrets panel folder

--- Snyk-Panel/Snyk-Credentials.js
+++ Snyk-Panel/Snyk-Credentials.js
@@ -1,4 +1,3 @@
-// Simulated database of users
 const users = [
     { id: 1, username: "user123", password: "Passw0rd!" },
     { id: 2, username: "bcastleberry", password: "Snyk123!" },
--- Snyk-Panel/Snyk-Secrets.js
+++ Snyk-Panel/Snyk-Secrets.js
@@ -1,4 +1,4 @@
-aws_access_key_id = AKIA2UC3BSXMODCO6HHQ
-aws_secret_access_key = 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
+aws_access_key_id = AKIA2UC3BSXMDKV6PUNU
+aws_secret_access_key = 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
 output = json
 region = us-east-2
